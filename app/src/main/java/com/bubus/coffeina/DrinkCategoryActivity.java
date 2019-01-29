2b5891909dfb9da8b0e964875aacb3977b7211ed
mzdrenka <zdrenka.mateusz@gmail.com>
2019-01-29 23:53:31 +0100

    Moja kofeina!

---
+++ app/src/main/java/com/bubus/coffeina/DrinkCategoryActivity.java
@@ -0,0 +1,13 @@
+package com.bubus.coffeina;
+
+import android.app.ListActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DrinkCategoryActivity extends ListActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
--- app/src/main/java/com/bubus/coffeina/TopLevelActivity.java
+++ app/src/main/java/com/bubus/coffeina/TopLevelActivity.java
@@ -1,7 +1,11 @@
 package com.bubus.coffeina;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 public class TopLevelActivity extends AppCompatActivity {
 
@@ -9,5 +13,20 @@ public class TopLevelActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_top_level);
+
+        // Tworzymy obiekt nasłuchujący OnItemClickListener
+        AdapterView.OnItemClickListener itemClickListener = new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if (position == 0){
+                    Intent intent = new Intent(TopLevelActivity.this, DrinkCategoryActivity.class);
+                    startActivity(intent);
+                }
+            }
+        }
+
+        // Dodajemy obiekt nasłuchujący do widoku listy
+        ListView listView = (ListView) findViewById(R.id.list_options);
+        listView.setOnItemClickListener(itemClickListener);
     }
 }
