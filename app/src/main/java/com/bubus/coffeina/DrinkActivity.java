80b60e151a875e48ca7af94bf73fa20950b56a95
mzdrenka <zdrenka.mateusz@gmail.com>
2019-02-15 00:28:13 +0100

    SPAC

--- app/src/main/java/com/bubus/coffeina/DrinkActivity.java
+++ app/src/main/java/com/bubus/coffeina/DrinkActivity.java
@@ -2,9 +2,13 @@ package com.bubus.coffeina;
 
 import android.annotation.TargetApi;
 import android.app.Activity;
+import android.content.ContentValues;
+import android.database.SQLException;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.RequiresApi;
+import android.view.View;
+import android.widget.CheckBox;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -19,14 +23,15 @@ import java.time.Instant;
 public class DrinkActivity extends Activity {
 
     public static final String EXTRA_DRINKNO ="drinkNo";
+
     @RequiresApi(api = Build.VERSION_CODES.P)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drink);
-    // Pobieramy identyfikator napoju z intencji
+     // Pobieramy identyfikator napoju z intencji
         int drinkNo = (Integer)getIntent().getExtras().get(EXTRA_DRINKNO);
-    // Tworzymy kursor.
+     // Tworzymy kursor.
         try {
             SQLiteOpenHelper coffeinaDatabaseHelper = new CoffeinaDatabaseHelper(this);
             SQLiteDatabase db = coffeinaDatabaseHelper.getReadableDatabase();
@@ -41,8 +46,8 @@ public class DrinkActivity extends Activity {
                 String nameText = cursor.getString(0);
                 String descriptionText = cursor.getString(1);
                 int photoId = cursor.getInt(2);
-
-                        // Wyświetlamy nazwę napoju
+                boolean isFavorite = (cursor.getInt(3) == 1);
+                // Wyświetlamy nazwę napoju
                 TextView name = (TextView)findViewById(R.id.name);
                 name.setText(nameText);
                 // Wyświetlamy opis napoju
@@ -55,6 +60,8 @@ public class DrinkActivity extends Activity {
                 photo.setContentDescription(nameText);
 
                 // Pobranie czy ulubiony
+                CheckBox favorite = (CheckBox)findViewById(R.id.favorite);
+                favorite.setChecked(isFavorite);
             }
 
             cursor.close();
@@ -62,6 +69,28 @@ public class DrinkActivity extends Activity {
         } catch(SQLiteException e) {
             Toast toast = Toast.makeText(this,  e.getMessage(),
                     Toast.LENGTH_SHORT);
+            toast.show();
+        }
+    }
+
+    // Aktualizujemy bazę danych po kliknięciu pola wyboru
+    public void onFavoriteClicked(View view){
+
+        int drinkNo = (Integer)getIntent().getExtras().get(EXTRA_DRINKNO);
+        CheckBox favorite = findViewById(R.id.favorite);
+        ContentValues drinkValues = new ContentValues();
+        drinkValues.put("FAVORITE", favorite.isChecked());
+        SQLiteOpenHelper coffeinaDatabaseHelper = new CoffeinaDatabaseHelper(DrinkActivity.this);
+        try {
+            SQLiteDatabase db = coffeinaDatabaseHelper.getWritableDatabase();
+            db.update(
+                    "DRINK",
+                    drinkValues,
+                    "_id = ?",
+                    new String[]{Integer.toString(drinkNo)});
+            db.close();
+        }catch (SQLException e){
+            Toast toast = Toast.makeText(this, "Baza danych jest niedostępna",Toast.LENGTH_SHORT);
             toast.show();
         }
     }
--- app/src/main/java/com/bubus/coffeina/TopLevelActivity.java
+++ app/src/main/java/com/bubus/coffeina/TopLevelActivity.java
@@ -1,14 +1,23 @@
 package com.bubus.coffeina;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.CursorAdapter;
 import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.Toast;
 
 public class TopLevelActivity extends AppCompatActivity {
-//strona 510
+//strona 529
+    private SQLiteDatabase db;
+    private Cursor favoritesCursor;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,5 +37,54 @@ public class TopLevelActivity extends AppCompatActivity {
         // Dodajemy obiekt nasłuchujący do widoku listy
         ListView listView = (ListView) findViewById(R.id.list_options);
         listView.setOnItemClickListener(itemClickListener);
+
+        // Zapisujemy na liście list_favorites ulubione napoje użytkownika
+        ListView listFavorites = (ListView)findViewById(R.id.list_favorites);
+        try{
+            SQLiteOpenHelper coffeinaDatabaseHelper = new CoffeinaDatabaseHelper(this);
+            db = coffeinaDatabaseHelper.getReadableDatabase();
+            favoritesCursor = db.query("DRINK", new String[] {"_id", "NAME"}, "FAVORITE = 1", null, null, null, null);
+            CursorAdapter favoriteAdapter = new SimpleCursorAdapter(
+                    TopLevelActivity.this, android.R.layout.simple_expandable_list_item_1, favoritesCursor, new String[] {"NAME"}, new int[]{android.R.id.text1}, 0);
+            listFavorites.setAdapter(favoriteAdapter);
+        }catch(SQLiteException e){
+            Toast toast = Toast.makeText(this, "Baza danych jest niedostępna", Toast.LENGTH_SHORT);
+            toast.show();
+        }
+        // Po kliknięciu ulubionego napoju przechodzimy do aktywności DrinkActivity
+        listFavorites.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(TopLevelActivity.this, DrinkActivity.class);
+                intent.putExtra(DrinkActivity.EXTRA_DRINKNO, (int) id);
+                startActivity(intent);
+            }
+        });
+    }
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        favoritesCursor.close();
+        db.close();
+    }
+
+    @Override
+    public void onRestart(){
+        super.onRestart();
+        try{
+            CoffeinaDatabaseHelper coffeinaDatabaseHelper = new CoffeinaDatabaseHelper(this);
+            db = coffeinaDatabaseHelper.getReadableDatabase();
+            Cursor newCursor = db.query("DRINK",new String[] { "_id", "NAME"},"FAVORITE = 1",null, null, null, null);
+            // Pobieramy adapter CursorAdapter używany przez widok ListView
+            ListView listVavorites = (ListView) findViewById(R.id.list_favorites);
+            CursorAdapter adapter = (CursorAdapter) listVavorites.getAdapter();
+            // Zmieniamy kursor używany przez adapter CursorAdapter na nowy
+            adapter.changeCursor(newCursor);
+
+            favoritesCursor = newCursor;
+        }catch (SQLiteException e){
+            Toast toast = Toast.makeText(this, "Baza danych jest niedostępna", Toast.LENGTH_SHORT);
+            toast.show();
+        }
     }
 }
